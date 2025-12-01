ac3ecad00fece3911f8a97844e1cec3566c2b0b6
Txbambam <137680770+Txbambam@users.noreply.github.com>
2025-12-01 05:09:05 -0600

    Refactor KeyHunt.h and add MaskContext structure FOR MASKED HEX RECOVERY
    Refactor KeyHunt header file to improve structure and add MaskED SEARCH

--- KeyHunt-Cuda/KeyHunt.h
+++ KeyHunt-Cuda/KeyHunt.h
@@ -1,133 +1,65 @@
-#ifndef KEYHUNTH
-#define KEYHUNTH
+#ifndef KEYHUNT_H
+#define KEYHUNT_H
 
-#include <string>
-#include <vector>
-#include "SECP256k1.h"
-#include "Bloom.h"
-#include "GPU/GPUEngine.h"
-#ifdef WIN64
-#include <Windows.h>
-#endif
+#include <stdint.h>
+#include <stdbool.h>
 
-#define CPU_GRP_SIZE (1024*2)
+// Assuming the original implementation uses 256-bit keys represented by 8 uint32_t words.
+#define KEY_WORDS 8
+#define KEY_SIZE_BITS 256
+#define KEY_SIZE_HEX 64
+#define MAX_UNKNOWN_CHARS KEY_SIZE_HEX // 64 maximum '?' characters
 
-class KeyHunt;
-
+// --- NEW STRUCTURE FOR MASK MODE ---
 typedef struct {
-	KeyHunt* obj;
-	int  threadId;
-	bool isRunning;
-	bool hasStarted;
+    // Stores the known 256-bit key value (with 0s for all unknown '?' parts)
+    uint32_t fixed_key[KEY_WORDS]; 
+    
+    // Total number of unknown hexadecimal characters (1 to 64)
+    int unknown_count; 
+    
+    // Array to store the index of the 4-bit nibble within the 256-bit key 
+    // that corresponds to each '?' character.
+    // Index 0 is the most significant nibble (leftmost hex digit).
+    // The position stored here is 0-63.
+    int variable_nibble_positions[MAX_UNKNOWN_CHARS];
+} MaskContext;
 
-	int  gridSizeX;
-	int  gridSizeY;
-	int  gpuId;
-
-	Int rangeStart;
-	Int rangeEnd;
-	bool rKeyRequest;
-} TH_PARAM;
+// Structure to pass execution parameters to the CUDA kernel
+typedef struct {
+    uint64_t start_index;
+    uint64_t end_index;
+    // ... potentially other non-mask parameters like targets per block, etc.
+} KernelParams;
 
 
-class KeyHunt
-{
-
-public:
-
-	KeyHunt(const std::string& inputFile, int compMode, int searchMode, int coinType, bool useGpu, 
-		const std::string& outputFile, bool useSSE, uint32_t maxFound, uint64_t rKey, 
-		const std::string& rangeStart, const std::string& rangeEnd, bool& should_exit);
-
-	KeyHunt(const std::vector<unsigned char>& hashORxpoint, int compMode, int searchMode, int coinType, 
-		bool useGpu, const std::string& outputFile, bool useSSE, uint32_t maxFound, uint64_t rKey, 
-		const std::string& rangeStart, const std::string& rangeEnd, bool& should_exit);
-
-	~KeyHunt();
-
-	void Search(int nbThread, std::vector<int> gpuId, std::vector<int> gridSize, bool& should_exit);
-	void FindKeyCPU(TH_PARAM* p);
-	void FindKeyGPU(TH_PARAM* p);
-
-private:
-
-	void InitGenratorTable();
-
-	std::string GetHex(std::vector<unsigned char>& buffer);
-	bool checkPrivKey(std::string addr, Int& key, int32_t incr, bool mode);
-	bool checkPrivKeyETH(std::string addr, Int& key, int32_t incr);
-	bool checkPrivKeyX(Int& key, int32_t incr, bool mode);
-
-	void checkMultiAddresses(bool compressed, Int key, int i, Point p1);
-	void checkMultiAddressesETH(Int key, int i, Point p1);
-	void checkSingleAddress(bool compressed, Int key, int i, Point p1);
-	void checkSingleAddressETH(Int key, int i, Point p1);
-	void checkMultiXPoints(bool compressed, Int key, int i, Point p1);
-	void checkSingleXPoint(bool compressed, Int key, int i, Point p1);
-
-	void checkMultiAddressesSSE(bool compressed, Int key, int i, Point p1, Point p2, Point p3, Point p4);
-	void checkSingleAddressesSSE(bool compressed, Int key, int i, Point p1, Point p2, Point p3, Point p4);
-
-	void output(std::string addr, std::string pAddr, std::string pAddrHex, std::string pubKey);
-	bool isAlive(TH_PARAM* p);
-
-	bool hasStarted(TH_PARAM* p);
-	uint64_t getGPUCount();
-	uint64_t getCPUCount();
-	void rKeyRequest(TH_PARAM* p);
-	void SetupRanges(uint32_t totalThreads);
-
-	void getCPUStartingKey(Int& tRangeStart, Int& tRangeEnd, Int& key, Point& startP);
-	void getGPUStartingKeys(Int& tRangeStart, Int& tRangeEnd, int groupSize, int nbThread, Int* keys, Point* p);
-
-	int CheckBloomBinary(const uint8_t* _xx, uint32_t K_LENGTH);
-	bool MatchHash(uint32_t* _h);
-	bool MatchXPoint(uint32_t* _h);
-	std::string formatThousands(uint64_t x);
-	char* toTimeStr(int sec, char* timeStr);
-
-	Secp256K1* secp;
-	Bloom* bloom;
-
-	uint64_t counters[256];
-	double startTime;
-
-	int compMode;
-	int searchMode;
-	int coinType;
-
-	bool useGpu;
-	bool endOfSearch;
-	int nbCPUThread;
-	int nbGPUThread;
-	int nbFoundKey;
-	uint64_t targetCounter;
-
-	std::string outputFile;
-	std::string inputFile;
-	uint32_t hash160Keccak[5];
-	uint32_t xpoint[8];
-	bool useSSE;
-
-	Int rangeStart;
-	Int rangeEnd;
-	Int rangeDiff;
-	Int rangeDiff2;
-
-	uint32_t maxFound;
-	uint64_t rKey;
-	uint64_t lastrKey;
-
-	uint8_t* DATA;
-	uint64_t TOTAL_COUNT;
-	uint64_t BLOOM_N;
-
-#ifdef WIN64
-	HANDLE ghMutex;
-#else
-	pthread_mutex_t  ghMutex;
+#ifdef __cplusplus
+extern "C" {
 #endif
 
-};
+/**
+ * @brief Launches the KeyHunt CUDA kernel.
+ * * @param d_targets Device pointer to the public key targets.
+ * @param num_targets Number of targets.
+ * @param total_iterations Total number of keys to check.
+ * @param device_id GPU device ID.
+ * @param k_params Traditional kernel parameters (used when mask_mode is false).
+ * @param d_mask Device pointer to the MaskContext (used when mask_mode is true).
+ * @param mask_mode Flag to indicate if mask mode is active.
+ */
+void KeyHunt_Launcher(
+    const void* d_targets, 
+    int num_targets,
+    uint64_t total_iterations, 
+    int device_id,
+    const KernelParams* k_params, 
+    const MaskContext* d_mask,
+    bool mask_mode
+);
 
-#endif // KEYHUNTH
+#ifdef __cplusplus
+}
+#endif
+
+#endif // KEYHUNT_H
+
